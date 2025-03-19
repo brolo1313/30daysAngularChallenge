87746f9b4e50d2c3d345f853ce6cacea1bdea1bd
Andrew Yupin <brolo1341@gmail.com>
2025-03-19 12:42:14 +0200

    improved drum component to handle audio loading errors and improve UI responsiveness

--- src/app/components/drum/drum.component.ts
+++ src/app/components/drum/drum.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-drum',
@@ -6,31 +6,64 @@ import { Component } from '@angular/core';
   templateUrl: './drum.component.html',
   styleUrl: './drum.component.scss'
 })
-export class DrumComponent {
+export class DrumComponent implements OnInit {
   public drumsList = [
     {
       key: 'A',
       name: 'joke-drums',
-      audio: '/src/assets/audio/joke-drums.mp3'
+      audio: new Audio('assets/audio/joke-drums.mp3'),
+      isDisabled: false
     },
     {
       key: 'S',
       name: 'snare-drum',
-      audio: '/src/assets/audio/snare-drum.mp3'
+      audio: new Audio('assets/audio/snare-drum.mp3'),
+      isDisabled: false
     },
     {
       key: 'D',
       name: 'tom-hot',
-      audio: '/src/assets/audio/tom-hot.mp3'
+      audio: new Audio('assets/audio/tom-hot.mp3'),
+      isDisabled: false
     },
     {
       key: 'F',
       name: 'bell-ring',
-      audio: '/src/assets/audio/bell-ring.mp3'
+      audio: new Audio('assets/audio/bell-ring.mp3'),
+      isDisabled: false
+    },
+    {
+      key: 'G',
+      name: '',
+      audio: new Audio('bad-path/assets/audio/bell-ring.mp3'),
+      isDisabled: false
     }
   ]
 
-  handler(event: KeyboardEvent) {
-    console.log(event.key, event.keyCode);
+
+  @HostListener('document:keydown', ['$event'])
+  onKeydown(event: KeyboardEvent) {
+    const drum = this.drumsList.find(drum => drum.key === event.key.toUpperCase());
+    if (drum && !drum.isDisabled) {
+      console.log(event.code, event.keyCode, event);
+      drum.audio.play();
+    }
+
+  }
+
+  ngOnInit() {
+    this.drumsList.forEach(drum => {
+      drum.audio.oncanplaythrough = () => {
+        console.log(`Audio for ${drum.name} is ready to play.`);
+      };
+
+      drum.audio.onerror = (error) => {
+        console.error(`Error loading audio for ${drum.name}:`, error);
+        drum.isDisabled = true;
+      };
+
+      drum.audio.load();
+
+    });
   }
 }
