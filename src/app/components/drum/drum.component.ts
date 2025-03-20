c621af0c66f6358d150fab65e08676ebccfddced
Andrew Yupin <brolo1341@gmail.com>
2025-03-20 11:40:22 +0200

    refactor drum component for improved layout exception handling and code organization

--- src/app/components/drum/drum.component.ts
+++ src/app/components/drum/drum.component.ts
@@ -17,7 +17,7 @@ export class DrumComponent implements OnInit {
   private lastPressedKey: string | null = '';
   public isLatinLayout: boolean = true;
 
-  public textForException: string = 'Sorry, but this app only works with the Latin keyboard layout.';
+  public textForLayoutException: string = 'Sorry, but this app only works with the Latin keyboard layout.';
 
   public drumsList: Drum[] = [
     {
@@ -58,7 +58,7 @@ export class DrumComponent implements OnInit {
   onKeydown(event: KeyboardEvent) {
     this.detectLayout(event);
 
-    const drum = this.drumsList.find(drum => drum.key === event.key.toUpperCase());
+    const drum = this.findInDrumsListBuKey(event.key);
     this.lastPressedKey = event.key.toUpperCase();
 
     if (drum && this.lastPressedKey && this.lastPressedKey === event.key.toUpperCase()) {
@@ -72,28 +72,96 @@ export class DrumComponent implements OnInit {
     if (drum && !drum.isDisabled) {
       drum.audio.play();
 
-      const drumItemElement = this.drumItemsHtml.toArray().find((item, index) => {
-        return this.drumsList[index].key === event.key.toUpperCase();
-      });
+      const drumItemElement = this.findHtmlElementByDrumKey(event)
 
       if (drumItemElement) {
-        drumItemElement.nativeElement.classList.add('active');
+        this.handleAddOrRemoveClass(drumItemElement, 'active', true);
       }
     }
   }
 
   @HostListener('document:keyup', ['$event'])
   onKeyup(event: KeyboardEvent) {
-    const drumItemElement = this.drumItemsHtml.toArray().find((item, index) => {
-      return this.drumsList[index].key === event.key.toUpperCase();
-    });
+    const drumItemElement = this.findHtmlElementByDrumKey(event);
 
     if (drumItemElement) {
-      drumItemElement.nativeElement.classList.remove('active');
+      this.handleAddOrRemoveClass(drumItemElement, 'active', false);
     }
   }
 
   ngOnInit() {
+    this.initDrums();
+  }
+
+  public kickAndAddClass(event: MouseEvent) {
+    const clickedDrum = (event.currentTarget as HTMLElement).lastChild?.textContent;
+    const drum = this.findInDrumsListBuKey(clickedDrum!);
+
+    this.lastPressedKey = clickedDrum!.toUpperCase();
+
+    if (drum && this.lastPressedKey && this.lastPressedKey === clickedDrum!.toUpperCase()) {
+      drum?.audio.pause();
+      drum.audio.currentTime = 0;
+      this.lastPressedKey = null;
+    } else {
+      this.lastPressedKey = clickedDrum!.toUpperCase();
+    }
+
+    if (drum && !drum.isDisabled) {
+      drum.audio.play();
+
+      const drumItemElement = this.findHtmlElementByDrumKey(event)
+
+      if (drumItemElement) {
+        this.handleAddOrRemoveClass(drumItemElement, 'active', true);
+      }
+    }
+  }
+
+
+  public kickAndRemoveClass(event: any) {
+    const drumItemElement = this.findHtmlElementByDrumKey(event);
+
+    if (drumItemElement) {
+      this.handleAddOrRemoveClass(drumItemElement, 'active', false);
+    }
+  }
+
+  private findInDrumsListBuKey(key: string) {
+    return this.drumsList.find(drum => drum.key === key!.toUpperCase());
+  }
+
+  private findHtmlElementByDrumKey(event: KeyboardEvent | MouseEvent) {
+    if ('key' in event) {
+      return this.drumItemsHtml.toArray().find((item, index) => {
+        return this.drumsList[index].key === event.key.toUpperCase();
+      });
+    } else {
+      const clickedDrum = (event.currentTarget as HTMLElement).lastChild?.textContent;
+      return this.drumItemsHtml.toArray().find((_, index) => {
+        return this.drumsList[index].key === clickedDrum!.toUpperCase();
+      });
+    }
+  }
+
+  private handleAddOrRemoveClass(element: ElementRef, className: string, isAdd: boolean) {
+    return isAdd
+      ? element.nativeElement.classList.add(className)
+      : element.nativeElement.classList.remove(className);
+  }
+
+  private detectLayout(event: KeyboardEvent): void {
+    const pressedKey = event.key;
+    if (/^[a-zA-Z]$/.test(pressedKey)) {
+      this.isLatinLayout = true;
+    } else if (/^[\u0400-\u04FF]$/.test(pressedKey)) { // checking Unicode for correct matching with Cyrillic 
+      this.isLatinLayout = false;
+    } else {
+      return;
+    }
+  }
+
+  private initDrums() {
     this.drumsList.forEach(drum => {
       drum.audio.oncanplaythrough = () => {
         console.log(`Audio for ${drum.name} is ready to play.`);
@@ -106,55 +174,5 @@ export class DrumComponent implements OnInit {
 
       drum.audio.load();
     });
-  }
-
-  public kickAndAddClass(event: any) {
-    const clickedDrum = event.currentTarget.lastChild.innerHTML;
-
-    const drum = this.drumsList.find(drum => drum.key === clickedDrum.toUpperCase());
-    this.lastPressedKey = clickedDrum.toUpperCase();
-
-    if (drum && this.lastPressedKey && this.lastPressedKey === clickedDrum.toUpperCase()) {
-      drum?.audio.pause();
-      drum.audio.currentTime = 0;
-      this.lastPressedKey = null;
-    } else {
-      this.lastPressedKey = clickedDrum.toUpperCase();
-    }
-
-    if (drum && !drum.isDisabled) {
-      drum.audio.play();
-
-      const drumItemElement = this.drumItemsHtml.toArray().find((item, index) => {
-        return this.drumsList[index].key === clickedDrum.toUpperCase();
-      });
-
-      if (drumItemElement) {
-        drumItemElement.nativeElement.classList.add('active');
-      }
-    }
-  }
-
-
-  public kickAndRemoveClass(event: any) {
-    const clickedDrum = event.currentTarget.lastChild.innerHTML;
-    const drumItemElement = this.drumItemsHtml.toArray().find((item, index) => {
-      return this.drumsList[index].key === clickedDrum.toUpperCase();
-    });
-
-    if (drumItemElement) {
-      drumItemElement.nativeElement.classList.remove('active');
-    }
-  }
-
-  private detectLayout(event: KeyboardEvent): void {
-    const pressedKey = event.key;
-    if (/^[a-zA-Z]$/.test(pressedKey)) {
-      this.isLatinLayout = true;
-    } else if (/^[\u0400-\u04FF]$/.test(pressedKey)) { // checking Unicode for correct matching with Cyrillic 
-      this.isLatinLayout = false;
-    } else {
-      return;
-    }
   }
 }
