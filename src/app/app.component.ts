737a9e68e234465fef3e6c4e01c07c5982222a30
Andrew Yupin <brolo1341@gmail.com>
2025-03-21 11:44:16 +0200

    add rate component with basic structure and routing, rewritten routing logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
   title = '30daysAngularChallenge';
+
+  public isHomePage = true;
+
+  public navLinks = [
+    {
+      name: 'Drum', path: '/drum'
+    },
+    {
+      name: 'Rate', path: '/rate'
+    }
+  ]
+
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.isHomePage = this.router.url === '/';
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'drum',
         loadComponent: () => import('./components/drum/drum.component').then((m) => m.DrumComponent)
-    }
+    },
+    {
+        path: 'rate',
+        loadComponent: () => import('./components/rate/rate.component').then((m) => m.RateComponent)
+    },
+
+    { path: '', redirectTo: '', pathMatch: 'full' },
+
+
 ];
