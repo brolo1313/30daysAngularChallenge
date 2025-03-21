25df242f32af70bd9a5677ba722bcff3126221e9
Andrew Yupin <brolo1341@gmail.com>
2025-03-21 12:23:27 +0200

    implement rate component with dynamic star rating and input options

--- src/app/components/rate-form/rate-form.component.ts
+++ src/app/components/rate-form/rate-form.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RateComponent } from '../../shared/components/rate/rate.component';
 
 @Component({
   selector: 'app-rate-form',
-  imports: [],
+  imports: [RateComponent],
   templateUrl: './rate-form.component.html',
   styleUrl: './rate-form.component.scss'
 })
 export class RateFormComponent {
 
+  public rateOptions = {
+    size: 10,
+    text: 'Rate this'
+  }
 }
--- src/app/shared/components/rate/rate.component.ts
+++ src/app/shared/components/rate/rate.component.ts
@@ -1,11 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+interface RateOptions {
+  size: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-rate',
   imports: [],
   templateUrl: './rate.component.html',
-  styleUrl: './rate.component.scss'
+  styleUrl: './rate.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: RateComponent,
+      multi: true,
+    }
+  ],
 })
-export class RateComponent {
+export class RateComponent implements ControlValueAccessor, OnInit {
+
+  @Input() options!: RateOptions;
+
+  public  trackByIndex(index: number): number {
+    return index;
+  }
+
+  public stars: any[] = [];
+  public rate = 0;
+  public disabled = false;
+  public onChange: any = () => { };
+  public onTouched: any = () => { };
+
+  public writeValue(value: number): void {
+    this.rate = value;
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  ngOnInit() {
+    console.log('RateComponent', this.options);
+    this.fillStarts();
+    console.log('RateComponent', this.stars);
+  }
+
+  private fillStarts(): void{
+    // this.stars = Array.from({ length: this.options.size })
+    while (this.stars.length < this.options.size) {
+      this.stars.push({
+        id: this.stars.length,
+      });
+    }
+  }
+
+  public rateUp(): void {
+    if (this.rate < this.options.size) {
+      console.log('');
+      // this.rate++;
+      // this.onChange(this.rate);
+    }
+  }
+
+  public rateDown(): void {
+    if (this.rate > 0) {
+      this.rate--;
+      this.onChange(this.rate);
+    }
+  }
 
 }
